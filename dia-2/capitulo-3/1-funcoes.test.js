d40b371bb684568a8427a7037d2d95e2aa1ce91b
Fábio Borges Júnior <salazenas@outlook.com>
2018-07-18 21:27:20 -0300

    Capitulo 2 e um plus do capítulo 3

--- dia-2/capitulo-3/1-funcoes.test.js
+++ dia-2/capitulo-3/1-funcoes.test.js
@@ -1,7 +1,9 @@
 describe("Funções", function () {
   it("Implemente uma função chamada add que recebe dois parâmtros e retorna a soma deles", function () {
 
-    // IMPLEMENTE
+  function add (x,y) {
+    return x + y;
+  }
 
     expect(add(1, 2)).toEqual(3);
   });
@@ -16,9 +18,9 @@ describe("Funções", function () {
         var z = 30
         valor = x + y + z
       }
-      valor = x + z
+      valor = x + z 
 
-      var RESPOSTA = // RESPOSTA
+      var RESPOSTA = 40;
       expect(valor).toEqual(RESPOSTA)
     })
 
